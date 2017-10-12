c44e6d2b6af715b15d1dbe142da8446506b2d17e
Administrator <admin@example.com>
2017-10-12 14:05:06 +0800

    堆栈学习

--- src/com/hanghang/test/StackTest.java
+++ src/com/hanghang/test/StackTest.java
@@ -4,6 +4,23 @@ import java.util.Stack;
 
 /**
  * Created by zjm on 2017/10/10.
+ * 1. public push  （item ）  把项 压入栈顶。其作用与 addElement (item ) 相同。
+
+ 　　　　　　参数 item 压入栈顶的项 。 返回： item 参数 ；
+
+ 　　　　2. public pop () 移除栈顶对象，并作为函数的值 返回该对象。
+
+ 　　　　　　返回：栈顶对象（Vector 对象的中的最后一项）。
+
+ 　　　　　　抛出异常 ： EmptyStackException 如果堆栈式空的 。。。
+
+ 　　　　3. public peek() 查看栈顶对象而不移除它。。
+
+ 　　　　　　返回：栈顶对象（Vector 对象的中的最后一项）。
+
+ 　　　　　　抛出异常 ： EmptyStackException 如果堆栈式空的 。。。
+
+ 　　　　4. public boolean empty （测试堆栈是否为空。）
  */
 public class StackTest {
     public static void main(String[] args) {
